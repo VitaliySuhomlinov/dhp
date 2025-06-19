45129e55507ada4914fba5df5074ff32d289cd53
VitaliySuhomlinov <scriptcraft.site@gmail.com>
2025-06-19 14:46:32 +0300

    patch 5 func sleep

--- dhp.go
+++ dhp.go
@@ -15,7 +15,7 @@ func VarExport(value interface{}) {
 // -----[ PRINT HELPERS ]-----
 // Sleep - более лаконичная обертка 
 func Sleep(countSeconds int) {
-	time.Sleep(time.Duration(countSeconds) * time.Second)
+	time.Sleep(time.Second * time.Duration(countSeconds))
 }
 
 // -----[ PRINT HELPERS ]-----
